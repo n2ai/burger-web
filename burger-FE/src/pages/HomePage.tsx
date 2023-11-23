5953c1f14c669c0db10917c90d0d3730d9c0e2c2
n2ai <trinhh7@gator.uhd.edu>
2023-11-23 15:56:29 -0600

    Update

--- burger-FE/src/pages/HomePage.tsx
+++ burger-FE/src/pages/HomePage.tsx
@@ -2,9 +2,9 @@ import '../styles/homePage.css'
 
 const HomePage = ()=>{
     return(
-        <div className="homepage-main">
+        <div className="homepage-main w-[100vw] h-[100vh] bg-black">
             <div className="homepage-reservation">
-                
+                <button className='text-white'>Reserve Table</button>
             </div>
         </div>
     )
