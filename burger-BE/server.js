09f1ab654221042f480ecdea021bd9910780dd29
n2ai <trinhh7@gator.uhd.edu>
2023-12-04 19:31:41 -0600

    Configured Backend

--- burger-BE/server.js
+++ burger-BE/server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+//Configuring the express
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
-app.post('http://localhost:5173/reservation',(req,res)=>{
-  res.send(req)
-  console.log(req)
+app.post('/reservation',async(req,res)=>{
+  try{
+    await res.send(JSON.stringify('Backend have received'))
+    
+  }catch(err){
+    res.status(404).send(`Here is the error: ${err}`)
+  }
+  
 })
 
 app.get('/', (req, res) => {
