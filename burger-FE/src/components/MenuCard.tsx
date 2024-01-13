0daa5859611052b7633a60ba8335b542939ae5f8
n2ai <trinhh7@gator.uhd.edu>
2024-01-12 19:38:41 -0600

    Update Menu card

--- burger-FE/src/components/MenuCard.tsx
+++ burger-FE/src/components/MenuCard.tsx
@@ -7,24 +7,30 @@ interface IMenuCard{
 
 const MenuCard:React.FC<IMenuCard> = ({burgerName,burgerIngredients,burgerImgUrl,burgerPrice})=>{
     
-    {/**Please remove the boder in the card after finishing responsive */}
+    {/**Please remove the border in the card after finishing responsive */}
     return(
         
-        <div className="w-[90%] mt-[5rem] flex flex-wrap justify-center border border-white">
-            <div className="max-md:w-full max-md:text-center">
-                <h1>{burgerName}</h1>
+        <div className="w-[90%] mt-[5rem] flex max-md:flex-wrap max-md:justify-center">
+            <div className="flex flex-wrap justify-center ">
+                <div className="w-full text-center">
+                    <h1>{burgerName}</h1>
+                </div>
+                <div className=" max-md:w-[60%] w-[50%]">
+                    <img className="object-cover" src={`/src/images/${burgerImgUrl}.png`} alt={burgerImgUrl} ></img> 
+                </div>
             </div>
-            <div className="w-[60%]">
-                <img className="object-cover" src={`/src/images/${burgerImgUrl}.png`} alt={burgerImgUrl} ></img>
+            
+            <div>
+                <div className="max-md:w-full max-md:text-center">
+                    <p>Include: {burgerIngredients}</p>
                 
+                </div>
+
+                <div className="max-md:w-full max-md:text-center">
+                    <p>Price: {burgerPrice}</p>
+                </div>
             </div>
-            <div className="max-md:w-full max-md:text-center">
-                <p>Include: {burgerIngredients}</p>
-                
-            </div>
-            <div className="max-md:w-full max-md:text-center">
-                <p>Price: {burgerPrice}</p>
-            </div>
+            
         </div>
     )
 }
